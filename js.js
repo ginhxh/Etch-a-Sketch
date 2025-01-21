d57d0262eb2b3c8253eec4ec46602a98b899251e
Ahmd Rabar <ahmedrbr010@gmail.comclcleardd>
2025-01-21 22:09:19 +0100

    finished project

---
+++ js.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+let colorHex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "a", "b", "c", "d", "e", "f"];
+
+btn.addEventListener("click", () => {
+  let divSize = input.value;
+
+  if (divSize < 100 && divSize > 0) {
+    container.innerHTML = "";
+    for (let i = 0; i < divSize * divSize; i++) {
+      const div = document.createElement("div");
+      div.setAttribute("class", "div");
+      container.append(div);
+    }
+
+    const div = document.querySelectorAll(".div");
+
+    div.forEach((divv) => {
+      divv.addEventListener("mouseover", () => {
+        let color = "#";
+
+        for (let i = 0; i < 6; i++) {
+          let rndomIndex = Math.floor(Math.random() * colorHex.length);
+          let letterColor = colorHex[rndomIndex];
+          color += letterColor;
+          console.log(rndomIndex);
+          console.log(letterColor);
+          console.log(color);
+        }
+        divv.style.backgroundColor = color;
+      });
+    });
+  }
+});
